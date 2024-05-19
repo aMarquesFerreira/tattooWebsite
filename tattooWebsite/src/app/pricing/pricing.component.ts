2cb96c88b3952596c891c46cee470ca5c4adb3ae
AndreFeMarques <1200605@isep.ipp.pt>
2024-05-19 10:15:22 +0100

    pricing done

--- tattooWebsite/src/app/pricing/pricing.component.ts
+++ tattooWebsite/src/app/pricing/pricing.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.css']
 })
-export class PricingComponent {
+export class PricingComponent implements AfterViewInit{
+
+  isResponsive: boolean = false;
+
+  ngAfterViewInit() {
+    this.checkResponsive(window.innerWidth);
+  }
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkResponsive(event.target.innerWidth);
+  }
+
+  checkResponsive(width: number) {
+    this.isResponsive = width < 1090; // Defina o limite para telas responsivas
+  }
 
 }
